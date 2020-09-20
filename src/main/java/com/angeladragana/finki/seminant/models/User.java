bb14346249358bb832df7856f3ce922b93d53345
anagardt <tasevska.d@yahoo.com>
2020-09-20 20:28:17 +0200

    bug fix

--- src/main/java/com/angeladragana/finki/seminant/models/User.java
+++ src/main/java/com/angeladragana/finki/seminant/models/User.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import javax.persistence.*;
 import java.util.Date;
 import java.util.Set;
+import java.util.TreeSet;
 
 @Entity
 @Data
@@ -31,5 +32,5 @@ public class User {
 
     @JsonIgnore
     @OneToMany(mappedBy = "createdBy")
-    private Set<Query> createdQueries;
+    private Set<Query> createdQueries = new TreeSet<>();
 }
